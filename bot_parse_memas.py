cfd3be4939750d7f657e10fac8d7fd7ad7f6b089
Samund <samylolik@ya.ru>
2018-07-29 16:07:31 +0300

    Webhooks

--- bot_parse_memas.py
+++ bot_parse_memas.py
@@ -142,7 +142,7 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    #updater.start_polling()
+    updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
